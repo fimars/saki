04d7b1fcae9c88d4c4069a0e340194f55725d5c5
fimars <foru.fy@gmail.com>
2019-03-08 15:36:33 +0800

    add themeConfig

--- hell.config.js
+++ hell.config.js
@@ -5,5 +5,11 @@ module.exports = {
   ignores: ["README.md", "ignores/*"],
   head: [
     ['link', { rel: 'icon', href: './favicon.png' }]
-  ]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '关于我', link: '/' },
+      { text: '关于HellDoc', link: '/About Hell' }
+    ]
+  }
 }
